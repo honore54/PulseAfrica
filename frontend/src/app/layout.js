f8a1ed764c7fa5ab82fd1d28007110d20752d924
Honore <your@email.com>
2026-04-06 20:46:02 +0200

    feat: add Adsterra ads (Native Banner, 300x250, 320x50) alongside AdSense

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -28,6 +28,45 @@ export default function RootLayout({ children }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5287640072959856"
           crossOrigin="anonymous"
         />
+
+        {/* Adsterra Native Banner */}
+        <script
+          async
+          data-cfasync="false"
+          src="https://pl29072546.profitablecpmratenetwork.com/6504f9b6647c3fbed07e1e8d57f0d892/invoke.js"
+        />
+
+        {/* Adsterra Banner 300x250 */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              atOptions = {
+                'key' : '88a98201e929bca18285796130de047d',
+                'format' : 'iframe',
+                'height' : 250,
+                'width' : 300,
+                'params' : {}
+              };
+            `
+          }}
+        />
+        <script src="https://www.highperformanceformat.com/88a98201e929bca18285796130de047d/invoke.js" async />
+
+        {/* Adsterra Banner 320x50 mobile */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              atOptions = {
+                'key' : '5dab8a6165ad6f6c7573b276bf447626',
+                'format' : 'iframe',
+                'height' : 50,
+                'width' : 320,
+                'params' : {}
+              };
+            `
+          }}
+        />
+        <script src="https://www.highperformanceformat.com/5dab8a6165ad6f6c7573b276bf447626/invoke.js" async />
       </head>
       <body>
         {/* Google Analytics */}
@@ -52,6 +91,13 @@ export default function RootLayout({ children }) {
           <div className="h-rose" />
           <div className="h-jade" />
         </div>
+
+        {/* Adsterra Native Banner container */}
+        <div
+          id="container-6504f9b6647c3fbed07e1e8d57f0d892"
+          style={{ textAlign: 'center', margin: '0 auto' }}
+        />
+
         {children}
         <CookieBanner />
       </body>
